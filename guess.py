fa635710c41577ea225da691da120c270741171e
xookerchang <e84991211@gmail.com>
2020-11-03 23:31:12 +0800

    start and end

--- guess.py
+++ guess.py
@@ -1,6 +1,10 @@
-import random
 
-r = random.randint(1,100)
+import random
+start = input('請決定隨機數字範圍起始數值：')
+end = input('請決定隨機數字範圍結束數值：')
+start = int(start)
+end = int(end)
+r = random.randint(start,end)
 count = 0
 
 while True:
